33b9334a615ecfa2f79f3f7a37a62a223176e6ec
Mahmud Allahverdiyev <tr0j4n034@gmail.com>
2019-03-20 11:26:17 +0300

    file updates

--- .ipynb_checkpoints/mlp_dt-checkpoint.py
+++ .ipynb_checkpoints/mlp_dt-checkpoint.py
@@ -1,8 +1,16 @@
-from sklearn.neural_network import MLPClassifier, MLPRegressor
 import numpy as np
+import random
+
+from sklearn.neural_network import MLPClassifier, MLPRegressor
 
 class DecisionTreeClassifier():
-    def __init__(self, X=None, y=None, depth=1, min_leaf=10):
+    def __init__(self, 
+                 X=None, 
+                 y=None, 
+                 depth=1, 
+                 min_leaf=10, 
+                 threshold=0.5, 
+                 mlp_features_ratio=1.):
         self.X = X
         self.y = y
         self.n = 0 if X is None else X.shape[0]
@@ -11,31 +19,41 @@ class DecisionTreeClassifier():
         self.inner_model = None
         self.lhs = None
         self.rhs = None
-        self.threshold = 0.5
+        self.threshold = threshold
+        self.mlp_features_ratio = mlp_features_ratio
         
     def set_data(self, X, y):
         self.X = X
         self.y = y
         self.n = X.shape[0]
+    
+    def get_tree_depth():
+        return self.depth
+    
+    def get_tree_min_leaves():
+        return self.min_leaf
+    
     def fit(self, X, y, inner_model=MLPClassifier(solver='lbfgs', hidden_layer_sizes=(50,)), 
                         threshold=0.5, split_ratio=None):
         self.inner_model = inner_model
         if X is not None:
             self.set_data(X, y)
             self.threshold = threshold
+            self.mlp_features_indices = np.sort(np.array(
+                            random.sample(range(X.shape[1]), k=max(2, int(np.ceil(self.mlp_features_ratio * X.shape[1]))))))
         if X.shape[0] == 0:
             return
-        inner_model.fit(X, y)
+        inner_model.fit(X[:, self.mlp_features_indices], y)
         if self.depth == 0 or self.min_leaf >= self.n:
             return
         if split_ratio is None:
-            routing_probs = (inner_model.predict_proba(X)[:, 0] >= threshold).astype(int)
+            routing_probs = (inner_model.predict_proba(X[:, self.mlp_features_indices])[:, 0] >= threshold).astype(int)
             X_left = X[np.where(routing_probs == 0)[0]]
             X_right = X[np.where(routing_probs == 1)[0]]
             y_left = y[np.where(routing_probs == 0)[0]]
             y_right = y[np.where(routing_probs == 1)[0]]
         else:
-            routing_probs = inner_model.predict_proba(X)[:, 0]
+            routing_probs = inner_model.predict_proba(X[:, self.mlp_features_indices])[:, 0]
             ord_indices = np.argsort(routing_probs)
             left_indices = ord_indices[:-int(split_ratio * X.shape[0])]
             right_indices = ord_indices[-int(split_ratio * X.shape[0]):]
@@ -43,15 +61,14 @@ class DecisionTreeClassifier():
             X_right = X[right_indices]
             y_left = y[left_indices]
             y_right = y[right_indices]
-            
-        self.lhs = DecisionTreeClassifier(X_left, y_left, self.depth - 1, self.min_leaf)
-        self.rhs = DecisionTreeClassifier(X_right, y_right, self.depth - 1, self.min_leaf)
+        
+        self.lhs = DecisionTreeClassifier(X=X_left, y=y_left, depth=self.depth - 1, min_leaf=self.min_leaf, mlp_features_ratio=self.mlp_features_ratio)
+        self.rhs = DecisionTreeClassifier(X=X_right, y=y_right, depth=self.depth - 1, min_leaf=self.min_leaf, mlp_features_ratio=self.mlp_features_ratio)
         self.lhs.fit(X_left, y_left, inner_model, threshold, split_ratio)
         self.rhs.fit(X_right, y_right, inner_model, threshold, split_ratio)
-        return self.lhs, self.rhs
     
     def predict(self, X):
-        return np.array([self.predict_instance(X[i:i + 1]) for i in range(len(X))])
+        return np.array([self.predict_instance(X[i:i + 1, self.mlp_features_indices]) for i in range(len(X))])
     
     def predict_instance(self, X):
         output = self.inner_model.predict(X)
